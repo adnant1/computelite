c801a3dca81e6691f78b24ee01f1a6570659dd56
adnant1 <adnantayib8@gmail.com>
2025-12-19 23:10:39 -0500

    feat: implemented a fifo queue for jobs

--- pkg/model/cluster.go
+++ pkg/model/cluster.go
@@ -2,6 +2,6 @@ package model
 
 type ClusterState struct {
 	Nodes		map[string]*Node // Mapping of node IDs to Node structs
-	PendingJobs	[]*Job           // List of pending jobs in the cluster
+	PendingJobs	*JobQueue        // List of pending jobs in the cluster
 	RunningJobs	map[int64]*Node  // Mapping of running job IDs to their assigned nodes
 }
---
+++ pkg/model/job_queue.go
@@ -0,0 +1,38 @@
+package model
+
+// JobQueue represents a simple FIFO queue for jobs
+type JobQueue struct {
+	jobs []*Job
+}
+
+// Enqueue adds a job to the end of the queue
+func (q *JobQueue) Enqueue(job *Job) {
+	q.jobs = append(q.jobs, job)
+}
+
+// Dequeue removes and returns the job at the front of the queue
+func (q *JobQueue) Dequeue() (*Job, bool) {
+	if len(q.jobs) == 0 {
+		return nil, false
+	}
+
+	job := q.jobs[0]
+	q.jobs = q.jobs[1:]
+
+	return job, true
+}
+
+// Peek returns the job at the front of the queue without removing it
+func (q *JobQueue) Peek() (*Job, bool) {
+	if len(q.jobs) == 0 {
+		return nil, false
+	}
+
+	job := q.jobs[0]
+	return job, true
+}
+
+// Size returns the number of jobs in the queue
+func (q *JobQueue) Size() int {
+	return len(q.jobs)
+}
