cd3f0d197e15934b0d474ac7e250429ea89226f1
adnant1 <adnantayib8@gmail.com>
2025-12-19 23:13:44 -0500

    feat: implemented SubmitJob API to add jobs to the cluster

--- pkg/model/cluster.go
+++ pkg/model/cluster.go
@@ -5,3 +5,10 @@ type ClusterState struct {
 	PendingJobs	*JobQueue        // List of pending jobs in the cluster
 	RunningJobs	map[int64]*Node  // Mapping of running job IDs to their assigned nodes
 }
+
+// SubmitJob adds a new job to the pending jobs queue
+// for now assume all invariants are true
+// does not schedule any jobs, only adds to the JobQueue
+func (cs *ClusterState) SubmitJob(job *Job) {
+	cs.PendingJobs.Enqueue(job)
+}
