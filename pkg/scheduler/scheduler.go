e78f120f764f3bc7e0ce3965a46cd16ffda5f398
adnant1 <adnantayib8@gmail.com>
2025-12-20 21:35:12 -0500

    refactor: move SubmitJob method into the Scheduler

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -6,3 +6,10 @@ type Scheduler struct {
 	Cluster     *cluster.ClusterState // Reference to the cluster the scheduler manages
 	PendingJobs *JobQueue             // List of pending jobs waiting to be scheduled
 }
+
+// SubmitJob adds a new job to the pending jobs queue
+// does not schedule any jobs, only adds to the JobQueue
+func (s *Scheduler) SubmitJob(job *Job) {
+	job.State = Pending
+	s.PendingJobs.Enqueue(job)
+}
