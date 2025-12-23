382945071b57e230fd67da894f5a1ac91c24a589
adnant1 <adnantayib8@gmail.com>
2025-12-22 22:50:43 -0500

    feat: created constructor for ClusterState

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -5,6 +5,14 @@ type ClusterState struct {
 	RunningJobs	map[int64]*Node  // Mapping of running job IDs to their assigned nodes
 }
 
+// NewCluster initializes and returns a new ClusterState
+func NewCluster() *ClusterState {
+	return &ClusterState{
+		Nodes:		 make(map[string]*Node),
+		RunningJobs: make(map[int64]*Node),
+	}
+}
+
 // AddNode adds a new node to the cluster state
 func (cs *ClusterState) AddNode(node *Node) {
 	cs.Nodes[node.ID] = node
