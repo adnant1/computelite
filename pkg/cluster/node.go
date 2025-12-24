c7f03a5c457513795c211be662c560b9b1984ef6
adnant1 <adnantayib8@gmail.com>
2025-12-23 21:49:14 -0500

    feat: extended Node struct with NodeHealth enum + LastHeartbeat field

--- pkg/cluster/node.go
+++ pkg/cluster/node.go
@@ -1,9 +1,26 @@
 package cluster
 
-import "github.com/adnant1/computelite/pkg/resource"
+import (
+	"time"
+
+	"github.com/adnant1/computelite/pkg/resource"
+)
+
+type NodeHealth int
+
+// Define node health states
+const (
+	Unknown NodeHealth = iota
+	Healthy
+	Unhealthy
+)
 
 type Node struct {
-	ID            string            
+	ID            string
+
 	TotalCapacity resource.Resource // Total resource capacity of the node
 	Allocated     resource.Resource // Currently allocated resources on the node
+
+	Health        NodeHealth	    // Current health status of the node
+	LastHeartbeat time.Time         // Last heartbeat timestamp from the node
 }
