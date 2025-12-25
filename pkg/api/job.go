3bfd72164d3495cfbfc4e05b0f5058321cee4e4d
adnant1 <adnantayib8@gmail.com>
2025-12-25 13:00:28 -0500

    feat: added job run duration constant + extended Job struct

--- pkg/api/job.go
+++ pkg/api/job.go
@@ -1,5 +1,7 @@
 package api
 
+import "time"
+
 type JobState int
 
 // Define job states
@@ -12,8 +14,13 @@ const (
 	Evicted
 )
 
+const RunDuration = time.Minute * 1
+
 type Job struct {
-	ID       int64
-	Requires Resource // Resources required by the job
-	State    JobState          // Current state of the job
+	ID              int64
+	Requires        Resource   // Resources required by the job
+	State           JobState   // Current state of the job
+	AssignedNodeID  string     // ID of the node to which the job is assigned
+
+	StartedAt       time.Time  // Timestamp when the job started running
 }
