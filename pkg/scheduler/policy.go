3babe8c97ff04609d9b57730172cdc376788f719
adnant1 <adnantayib8@gmail.com>
2025-12-26 15:46:33 -0500

    feat: defined SchedulerPolicy interface

---
+++ pkg/scheduler/policy.go
@@ -0,0 +1,8 @@
+package scheduler
+
+import "github.com/adnant1/computelite/pkg/api"
+
+// SchedulerPolicy defines the interface for scheduling policies
+type SchedulerPolicy interface {
+	SelectNode(job *api.Job, nodes map[string]*api.Node) (string, bool)
+}
