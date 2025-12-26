9d2fc3a6c95fa21f0a39b7b6e38b74f310ddcb56
adnant1 <adnantayib8@gmail.com>
2025-12-25 22:43:40 -0500

    feat: added run and reconcile methods for ReschedulerController

--- pkg/controller/rescheduler_controller.go
+++ pkg/controller/rescheduler_controller.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"log"
 	"time"
 
+	"github.com/adnant1/computelite/pkg/api"
 	"github.com/adnant1/computelite/pkg/cluster"
 )
 
@@ -17,5 +19,33 @@ func NewReschedulerController(cs *cluster.ClusterState, interval time.Duration) 
 	return &ReschedulerController{
 		clusterState:      cs,
 		reconcileInterval: interval,
+	}
+}
+
+// Run begins watching and managing job rescheduling
+func (rc *ReschedulerController) Run() {
+	ticker := time.NewTicker(rc.reconcileInterval)
+	defer ticker.Stop()
+
+	// will soon add stop channel handling
+	for {
+		select {
+		case <-ticker.C:
+			rc.reconcile()
+		}
+	}
+}
+
+// reconcile checks for jobs on unhealthy nodes and reschedules them
+func (rc *ReschedulerController) reconcile() {
+	for jobID, job := range rc.clusterState.Jobs {
+		if job.AssignedNodeID != "" {
+			node, exists := rc.clusterState.Nodes[job.AssignedNodeID]
+
+			if exists && node.Health == api.Unhealthy {
+				rc.clusterState.EvictAndRequeueJob(jobID)
+				log.Printf("[rescheduler-controller] job=%d evicted from node=%s (node unhealthy)", jobID, job.AssignedNodeID)
+			}
+		}
 	}
 }
